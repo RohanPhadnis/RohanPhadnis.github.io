8934c5d1398c894321b7cac0536b33616b0bd974
RohanPhadnis <rohan.phadnis.usa@gmail.com>
2025-02-10 17:56:46 -0500

    100 word bio

--- script.js
+++ script.js
@@ -12,7 +12,40 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-// Sample data (replace this with your actual experiences data)
+/* ----- HOME PAGE -----*/
+const socialsData = [
+	{
+		"name": "LinkedIn",
+		"link": "https://www.linkedin.com/in/rohan-phadnis-0b82611b0/",
+		"svg": '<svg xmlns="http://www.w3.org/2000/svg" fill="white" width="64" height="64" viewBox="0 0 50 50"><path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z"></path></svg>',
+	},
+	{
+		"name": "GitHub",
+		"link": "https://github.com/RohanPhadnis",
+		"svg": '<svg role="img" fill="white" width="64" height="64" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>',
+	},
+	{
+		"name": "Gmail",
+		"link": "mailto:rohan.phadnis.usa@gmail.com",
+		"svg": '<svg role="img" fill="white" width="64" height="64" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Gmail</title><path d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z"/></svg>',
+	},
+]
+
+const socialsContainer = document.getElementById("socials");
+if (socialsContainer !== null) {
+	socialsData.forEach(social => {
+		
+		const link = document.createElement('a');
+		link.href = social.link;
+		link.innerHTML = social.svg;
+		link.style.margin = "15px";
+		socialsContainer.appendChild(link);
+		
+	});
+}
+
+
+/* ----- EXPERIENCES PAGE -----*/
 const experiencesData = [
   {
     "title": "Research Intern",
@@ -74,7 +107,7 @@ experiencesData.forEach(experience => {
 
 
 
-// Sample data (replace this with your actual experiences data)
+/* ----- PROJECTS PAGE -----*/
 const projectsData = [
   {
     "title": "Airavata MFT",
@@ -152,7 +185,7 @@ projectsData.forEach(project => {
 
 
 
-// Sample data (you will replace this with actual JSON data)
+/* ----- SKILLS PAGE -----*/
 const languagesData = [
   {
   	"name": "C",
@@ -181,7 +214,7 @@ const languagesData = [
   },
   {
   	"name": "Rust",
-  	"imageUrl": "https://upload.wikimedia.org/wikipedia/commons/d/d5/Rust_programming_language_black_logo.svg",
+  	"imageUrl": "https://www.rust-lang.org/logos/rust-logo-64x64.png",
   	"link": "https://rust-lang.org/"
   },
   {
@@ -191,7 +224,7 @@ const languagesData = [
   },
   {
     "name": "JavaScript",
-    "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+    "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/b/ba/Javascript_badge.svg",
     "link": "https://www.javascript.com"
   },
   {
