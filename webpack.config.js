b556cc017cc244da2880d5563da252a1d702c2f3
RohanPhadnis <rohan.phadnis.usa@gmail.com>
2025-02-09 23:10:13 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    hot: true,
+    client: {
+      webSocketURL: 'ws://localhost:8080/ws', // Ensures correct WebSocket communication
+    },
+  },
+};
+
